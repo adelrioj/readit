1c4a4ce8230f1642562d64f8a543a5a00f9eb792
adelrioj <alejandro@nextail.co>
2018-01-02 21:43:50 +0100

    pull from database

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -1,5 +1,7 @@
 class StoryController < ApplicationController
   def index
     @current_time = Time.now
+    @story = Story.find_by(name: 'SitePoint Forums')
+    @random_story = Story.order('RANDOM()').first
   end
 end
