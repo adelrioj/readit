000a68a618e2ef112430bdbe34681dc0c6a843bc
adelrioj <alejandro@nextail.co>
2018-01-13 20:17:18 +0100

    Revert "pull from database"
    This reverts commit 1c4a4ce8230f1642562d64f8a543a5a00f9eb792.

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -1,7 +1,5 @@
 class StoryController < ApplicationController
   def index
     @current_time = Time.now
-    @story = Story.find_by(name: 'SitePoint Forums')
-    @random_story = Story.order('RANDOM()').first
   end
 end
