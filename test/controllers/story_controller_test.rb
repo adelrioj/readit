df9ab717e90245df54f952f33f2c80369bdc1d04
adelrioj <alejandro@nextail.co>
2018-01-22 17:27:06 +0100

    add story completed

--- test/controllers/story_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StoryControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get story_index_url
-    assert_response :success
-  end
-
-end
