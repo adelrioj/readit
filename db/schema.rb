51613813832ce9de582ffb8302d0c4e1bf0f2999
adelrioj <alejandro@nextail.co>
2018-01-23 22:43:54 +0100

    add vote model and relationship with story

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226172130) do
+ActiveRecord::Schema.define(version: 20180123212837) do
 
   create_table "stories", force: :cascade do |t|
     t.string "name"
     t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "story_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
