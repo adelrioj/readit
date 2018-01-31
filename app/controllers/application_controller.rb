88e2c489909eda565424c8a06b3f3f9a417e85fd
adelrioj <alejandro@nextail.co>
2018-01-31 19:13:34 +0100

    add login restriction to view stories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,26 @@ class ApplicationController < ActionController::Base
 
   before_action :current_user
 
+  def ensure_login
+    return true if logged_in?
+    session[:return_to] = request.fullpath
+    # A filter that returns false halts the processing of any subsequent
+    # filters and exits the current controller method
+    redirect_to new_session_path and return false
+  end
+
   protected
 
   def current_user
     return unless session[:user_id]
     @current_user = User.where(id: session[:user_id]).first
   end
+
+  def logged_in?
+    !@current_user.nil?
+  end
+  # regular helpers are available only to views, and controller methods are
+  # available only to controllers. helper_method makes regular
+  # controller methods available to views as if they were regular helper methods
+  helper_method :logged_in?
 end
