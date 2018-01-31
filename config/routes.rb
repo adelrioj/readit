924364d653894cb9901ee0ad0c4baf8484dca4e8
adelrioj <alejandro@nextail.co>
2018-01-31 18:21:31 +0100

    add basic session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # When using the singular form, Rails knows we're talking about a singleton resource
+  resource :session
+
   resources :stories do
     resources :votes do
     end
