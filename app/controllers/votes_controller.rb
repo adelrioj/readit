18a242b0dd9264d905e4d42d9d4e22d9a8413b4a
adelrioj <alejandro@nextail.co>
2018-01-31 20:01:04 +0100

    fix user redirection after login

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,11 @@
 class VotesController < ApplicationController
   def create
     @story = Story.find(params[:story_id])
-    @story.votes.create
+    @story.votes.create(user: @current_user)
     respond_to do |format|
-      format.html { redirect_to @story, notice: 'Vote was successfully created.' }
+      format.html do
+        redirect_to @story, notice: 'Vote was successfully created.'
+      end
       format.js {}
     end
   end
