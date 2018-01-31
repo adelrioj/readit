6f59d9f6431db260a5d350faf1d4b128eed89480
adelrioj <alejandro@nextail.co>
2018-01-31 18:25:15 +0100

    add basic session controller II

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  before_action :current_user
+
+  def new
+  end
+
+  def create
+    @current_user = User.find_by(email: params[:email])
+    if @current_user && @current_user.authenticate(params[:password])
+    session[:user_id] = @current_user.id
+    redirect_to stories_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  protected
+
+  def current_user
+    return unless session[:user_id]
+    @current_user = User.where(id: session[:user_id]).first
+  end
+end
