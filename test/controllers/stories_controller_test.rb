c408c178ba0db0eb65b23b0c23a19e98867d9368
adelrioj <alejandro@nextail.co>
2018-01-31 20:19:36 +0100

    add User and Session to all needed controller tests

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
-
   test 'should get index' do
     get stories_url
     assert_response :success
   end
 
   test 'should get new' do
+    login_user
     get new_story_path
     assert_response :success
     assert_template 'new'
@@ -15,11 +15,13 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'new shows new form' do
+    login_user
     get new_story_path
     assert_select 'form div', count: 2
   end
 
   test 'adds a story' do
+    login_user
     assert_difference 'Story.count' do
       post stories_path, params: {
         story: {
@@ -33,6 +35,7 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'rejects when missing story attribute' do
+    login_user
     assert_no_difference 'Story.count' do
       post stories_path, params: {
         story: { name: 'story without a link' }
@@ -51,5 +54,37 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h2 span#vote_score'
     assert_select 'ul#vote_history li', count: 2
     assert_select 'div#vote_form form'
+  end
+
+  test 'show story submitter' do
+    get story_path(stories(:one))
+    assert_select 'p.submitted_by span', 'Glenn Goodrich'
+  end
+
+  test 'indicates not logged in' do
+    get stories_path
+    assert_select 'div#login_logout em', 'Not logged in.'
+  end
+
+  test 'show navigation menu' do
+    get stories_path
+    assert_select 'ul#navigation li', 2
+  end
+
+  test 'redirects if not logged in' do
+    get new_story_path
+    assert_response :redirect
+    assert_redirected_to new_session_path
+  end
+
+  test 'stores user with story' do
+    login_user
+    post stories_path, params: {
+      story: {
+        name: 'story with user',
+        link: 'http://www.story-with-user.com/'
+      }
+    }
+    assert_equal users(:glenn), Story.last.user
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,18 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml
+  # for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def login_user
+    post session_path, params: {
+      email: users(:glenn).email,
+      password: 'sekrit'
+    }
+  end
+
+  def logout_user
+    delete session_path
+  end
 end
